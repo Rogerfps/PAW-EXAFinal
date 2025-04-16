a9d0f828fafe3c5af68e83f97a55585e94be4f34
melicastro <melis.castro21@gmail.com>
2025-04-15 19:38:46 -0600

    ApplicationDbContext y program

--- PAW-Caso2/PAW-Caso2/Program.cs
+++ PAW-Caso2/PAW-Caso2/Program.cs
@@ -1,15 +1,42 @@
+using PAW_Caso2.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/api/events", async (ApplicationDbContext db) =>
+{
+    var eventos = await db.Eventos
+        .Where(e => e.Fecha >= DateTime.Now)
+        .Select(e => new {
+            e.Id,
+            e.Titulo,
+            e.Fecha,
+            e.Hora,
+            e.Ubicacion,
+            e.CupoMaximo
+        })
+        .ToListAsync();
+
+    return Results.Ok(eventos);
+});
+
+app.MapGet("/api/events/{id}", async (int id, ApplicationDbContext db) =>
+{
+    var evento = await db.Eventos.FindAsync(id);
+
+    if (evento == null)
+        return Results.NotFound();
+
+    return Results.Ok(evento);
+});
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
