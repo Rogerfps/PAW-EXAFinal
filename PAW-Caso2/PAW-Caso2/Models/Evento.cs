e430c87450d478ad51ca1f9768ca5990d9f1aff8
Rogerfps <rpereira40667@ufide.ac.cr>
2025-04-15 18:50:32 -0600

    Modelo evento

---
+++ PAW-Caso2/PAW-Caso2/Models/Evento.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PAW_Caso2.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan Hora { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La duración debe ser positiva.")]
+        public int Duracion { get; set; } // minutos
+
+        [Required]
+        public string Ubicacion { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "El cupo debe ser mayor a 0.")]
+        public int CupoMaximo { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public string UsuarioRegistro { get; set; }
+
+        
+        public Categoria Categoria { get; set; }
+    }
+}
