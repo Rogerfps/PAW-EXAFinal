ba4aeea909fcb31de48f926999d2f1ec2b6a3504
Mateo Rincón Gonzalez <127810785+MrShadowx199@users.noreply.github.com>
2025-04-15 19:20:34 -0600

    Modelo Asistencia

--- PAW-Caso2/PAW-Caso2/obj/Debug/net8.0/PAW-Caso2.AssemblyInfo.cs
+++ PAW-Caso2/PAW-Caso2/obj/Debug/net8.0/PAW-Caso2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c86dc95211bb254eeec6afad9325af0b6a73a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f8d987c0b1c9b9a2152f093fd5f626b399464e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
