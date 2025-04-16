0d99b19f5ac8cba808420bb03337c9ee7aa0418f
Kevin R. <37754575+kramirez8808@users.noreply.github.com>
2025-04-15 18:52:28 -0600

    Categoria Model

---
+++ PAW-Caso2/PAW-Caso2/Models/Categoria.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PAW_Caso2.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Estado { get; set; }
+
+        [Required]
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+        [Required]
+        public int UsuarioRegistro { get; set; }
+
+        public Usuario? Usuario { get; set; }
+
+        // Relacion con Eventos
+        public IEnumerable<Evento> Eventos { get; set; } = new List<Evento>();
+    }
+}
--- PAW-Caso2/PAW-Caso2/obj/Debug/net8.0/PAW-Caso2.AssemblyInfo.cs
+++ PAW-Caso2/PAW-Caso2/obj/Debug/net8.0/PAW-Caso2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed2433feb517c76a59a99af26f31bc12289f51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e52ef44b991c2d7bfd4b919851495ef755e7cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAW-Caso2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
