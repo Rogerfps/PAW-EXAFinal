993b64ddbe6e1bd44b06621670467c1388c7ae45
melicastro <melis.castro21@gmail.com>
2025-04-15 20:08:21 -0600

    API

--- PAW-Caso2/PAW-Caso2/Program.cs
+++ PAW-Caso2/PAW-Caso2/Program.cs
@@ -10,12 +10,19 @@ builder.Services.AddDbContext<EventCorpContext>(op =>
     op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();    
 }
 
 app.UseHttpsRedirection();
@@ -28,5 +35,34 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.MapGet("/api/events", async (EventCorpContext db) =>
+    {
+        var eventos = await db.Eventos
+            .Where(e => e.Fecha >= DateTime.Now)
+            .Select(e => new
+            {
+                e.Id,
+                e.Titulo,
+                e.Fecha,
+                e.Hora,
+                e.Ubicacion,
+                e.CupoMaximo
+            })
+            .ToListAsync();
+
+        return Results.Ok(eventos);
+    });
+
+    app.MapGet("/api/events/{id}", async (int id, EventCorpContext db) =>
+    {
+        var evento = await db.Eventos.FindAsync(id);
+
+        if (evento == null)
+            return Results.NotFound();
+
+        return Results.Ok(evento);
+    });
+
 
 app.Run();
