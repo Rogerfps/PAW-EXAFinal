c4c86dc95211bb254eeec6afad9325af0b6a73a2
Rogerfps <rpereira40667@ufide.ac.cr>
2025-04-15 19:02:09 -0600

    Inscripción de evento

---
+++ PAW-Caso2/PAW-Caso2/Models/Inscripcion.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PAW_Caso2.Models
+{
+    public class Inscripcion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int EventoId { get; set; }
+
+        [Required]
+        public string UsuarioId { get; set; }
+
+        public DateTime FechaInscripcion { get; set; }
+
+        // Relaciones
+        public Evento Evento { get; set; }
+        //public ApplicationUser Usuario { get; set; }
+    }
+}
